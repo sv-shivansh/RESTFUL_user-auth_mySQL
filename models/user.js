9a3a09f400a0201d9af09e75d0f55a8798e6a9d7
sv-shiansh <sv_shiansh@outlook.com>
2021-01-25 00:40:58 +0530

    final commit

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('<database_name>', '<user>', '<password>', {
+var sequelize = new Sequelize('user_auth', 'root', '1234', {
     host: 'localhost',
     dialect: 'mysql'});
 
